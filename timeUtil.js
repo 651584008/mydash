1f07f0b2d192fd344466e6657059b3416d18e98d
yunying.liang <651584008@qq.com>
2021-06-29 16:26:35 +0800

    Update timeUtil.js

--- timeUtil.js
+++ timeUtil.js
@@ -23,7 +23,42 @@ function formatDate(date) {
 	return [year, month, day].map(formatNumber).join('-')
 }
 
+//时间戳转换成日期时间
+function js_date_time(time, cFormat) {
+  if (arguments.length === 0) {
+    return null;
+  }
+  const format = cFormat || '{y}{m}{d}';
+  let date;
+  if (typeof time === 'object') {
+    date = time;
+  } else {
+    if (('' + time).length === 10) time = parseInt(time) * 1000
+    date = new Date(time);
+  }
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  }
+  const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+    if (formatObj.a == 0) formatObj.a = 7;
+    let value = formatObj[key];
+    if (key === 'a') return ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'][value - 1];
+    if (result.length > 0 && value < 10) value = '0' + value;
+    return value || 0;
+  });
+  return time_str;
+}
+
+
+
 module.exports = {
-	formatTime,
-	formatDate
+	formatTime, //时间转日期时间
+	formatDate,//时间转日期
+	js_date_time: js_date_time, //时间戳转日期时间
 }
